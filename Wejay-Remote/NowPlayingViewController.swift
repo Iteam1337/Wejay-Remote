49bd065ff466f290daff0ac564413d59225a0c06
Anders Bornholm <anders@bornholm.se>
2014-11-19 18:46:09 +0100

    Added a table view

---
+++ Wejay-Remote/NowPlayingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NowPlayingViewController.swift
+//  Wejay-Remote
+//
+//  Created by Anders Bornholm on 2014-11-19.
+//  Copyright (c) 2014 Iteam Solutions. All rights reserved.
+//
+
+import UIKit
+
+
+class NowPlayingViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
